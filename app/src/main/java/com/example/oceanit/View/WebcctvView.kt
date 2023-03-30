ba6cab9c5e8e8d3e3610dbaa7f9d307b7bc4dde0
JS <zks145@naver.com>
2023-03-30 19:27:31 +0900

    RoomDB를 이용해 WebView 자동 로그인

--- app/src/main/java/com/example/oceanit/View/WebcctvView.kt
+++ app/src/main/java/com/example/oceanit/View/WebcctvView.kt
@@ -4,15 +4,16 @@ import android.annotation.SuppressLint
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
+import androidx.room.Room
+import com.example.oceanit.DB.AppDatabase
 import com.example.oceanit.R
 
 class WebcctvView : AppCompatActivity() {
 
-    private var webView : WebView? = null
+    private var webView: WebView? = null
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,36 +23,41 @@ class WebcctvView : AppCompatActivity() {
         webView = findViewById(R.id.webView)
         val webSettings = webView!!.settings
 
+        val db = AppDatabase.getDBInstance(this.applicationContext)
+
         webView!!.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView, url: String) {
                 // 로그인 페이지 URL
                 if (url == "http://211.184.227.81:8500/login") {
                     view.loadUrl(
                         "javascript:(function() { " +
-                                "document.getElementsByName('id')[0].value='user1';" +
-                                "document.getElementsByName('pw')[0].value='user1';" +
+                                "document.getElementsByName('id')[0].value='" + "${db!!.UserDao()!!.getAll(1).loginId}';" +
+                                "document.getElementsByName('pw')[0].value='${db.UserDao()!!.getAll(1).loginPw}';" +
                                 "document.forms[0].submit();" +
                                 "})()"
                     )
                 } else if (url == "http://211.184.227.81:8500/cctvView") {
-
+                    // 로그인 후 CCTV 화면으로 이동
                 }
             }
         }
 
-//        webView.webViewClient = WebViewClient() //새창 열기 없이 웹뷰내에서 다시 열기
+        webView!!.webChromeClient = object : WebChromeClient() {
+            override fun onJsAlert(view: WebView, url: String, message: String, result: JsResult): Boolean {
+                // 자동 로그인 후 페이지 이동 처리
+                webView!!.loadUrl("http://211.184.227.81:8500/cctvView")
+                result.confirm()
+                return true
+            }
+        }
+
         webSettings.loadWithOverviewMode = true
         webSettings.javaScriptEnabled = true // allow the js
         webSettings.useWideViewPort = true // 화면 사이즈 맞추기 허용 여부
-        webSettings.setSupportZoom(true) // 화면 줌 허용 여부
-        webSettings.builtInZoomControls = true // 화면 확대 축소 허용 여부
+        webSettings.setSupportZoom(false) // 화면 줌 허용 여부
+        webSettings.builtInZoomControls = false // 화면 확대 축소 허용 여부
         webSettings.cacheMode = WebSettings.LOAD_NO_CACHE // 브라우저 캐시 허용 여부
         webSettings.domStorageEnabled = true
-
-
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON) //화면이 계속 켜짐
-
-        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_USER
 
         webView!!.loadUrl("http://211.184.227.81:8500/cctvView")
     }
